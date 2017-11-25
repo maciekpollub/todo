c36478684b71c7d281b38e59b6ff8bd86998d88c
Maciej Ziemba <maciek.ziemba@gmail.com>
2017-11-25 13:19:11 +0100

    a new rehearsal application triggered

---
+++ src/components/Dashboard.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>To nasz dashboard</h1>
+                <p>jest ok</p>
+                <div>
+                    <div>Widget1</div>
+                    <div>Widget2</div>
+                </div>
+                <div>
+                    <div>Widget3</div>
+                    <div>Widget4</div>
+                </div>
+            </div>
+        );
+
+    }
+}
+export default Dashboard;
