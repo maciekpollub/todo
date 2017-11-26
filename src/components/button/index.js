7df40cea507548d046287ff3e33e59a6f84fdd2a
Maciej Ziemba <maciek.ziemba@gmail.com>
2017-11-26 09:51:49 +0100

    external styling added...

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 
-
+import './index.css';//importujemy style z zewsątrz , z folderu daneg komponentu- tak jest lepiej...
 
 class Button extends Component {
     render() {
         const buttonStyle = {
             backgroundColor: 'green',
-            color: 'yellow'
+            color: 'yellow',
+            fontSize: 40
         }
         return(
-            <div>
+            <div className="btn-container">
                 <button style = {buttonStyle}>{this.props.label}</button>
             </div>
         )
